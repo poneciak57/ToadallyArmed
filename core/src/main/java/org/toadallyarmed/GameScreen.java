774b4cd31abb6071a4ae84a4775eae659bd1d145
SomePersonFromMars <42392219+SomePersonFromMars@users.noreply.github.com>
2025-05-04 18:49:49 +0200

    Basic screen setup and example rendering

---
+++ core/src/main/java/org/toadallyarmed/GameScreen.java
@@ -0,0 +1,82 @@
+package org.toadallyarmed;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class GameScreen implements Screen {
+    final Main main;
+    FitViewport viewport;
+
+    Texture backgroundTexture;
+    Texture testFrogTexture;
+    TextureRegion testFrogTextureRegion;
+
+    GameScreen(Main main) {
+        this.main = main;
+
+        viewport = new FitViewport(12, 6);
+        this.main.updateFontScale(viewport);
+
+        backgroundTexture = new Texture("GameScreen/background.jpg");
+        testFrogTexture = new Texture("GameScreen/Frogs/Wizard/FrogWizardAttack.png");
+        testFrogTextureRegion = new TextureRegion(testFrogTexture, 0, 0, 86, 66);
+    }
+
+    @Override
+    public void show() {
+        // Prepare your screen here.
+    }
+
+    @Override
+    public void render(float delta) {
+        // Draw your screen here. "delta" is the time since last render in seconds.
+
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        main.spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        main.spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        main.spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        main.spriteBatch.draw(testFrogTextureRegion, 0, 0, 1, 1);
+
+        main.font.draw(main.spriteBatch, "Hello World!", 0, worldHeight);
+
+        main.spriteBatch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your screen here. The parameters represent the new window size.
+        viewport.update(width, height, true);
+        main.updateFontScale(viewport);
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy screen's assets here.
+        backgroundTexture.dispose();
+        testFrogTexture.dispose();
+    }
+}
--- core/src/main/java/org/toadallyarmed/Main.java
+++ core/src/main/java/org/toadallyarmed/Main.java
@@ -1,11 +1,38 @@
 package org.toadallyarmed;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    SpriteBatch spriteBatch;
+    BitmapFont font;
+
+    void updateFontScale(Viewport viewport) {
+        //font has 15pt, but we need to scale it to our viewport by ratio of viewport height to screen height
+        font.setUseIntegerPositions(false);
+        font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
+    }
+
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        spriteBatch = new SpriteBatch();
+        font = new BitmapFont();
+
+        setScreen(new GameScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+        font.dispose();
     }
 }
