d2dec486c9f069354f8f738303a5803983d3cfce
poneciak57 <poneciak@gmail.com>
2025-05-09 12:32:01 +0200

    added HealthComponent as to base components registry

--- core/src/main/java/org/toadallyarmed/component/interfaces/BaseComponentsRegistry.java
+++ core/src/main/java/org/toadallyarmed/component/interfaces/BaseComponentsRegistry.java
@@ -1,11 +1,14 @@
 package org.toadallyarmed.component.interfaces;
 
+import org.toadallyarmed.component.HealthComponent;
+
 import java.util.List;
 
 public class BaseComponentsRegistry {
     public static List<Class<? extends Component>> BASE_COMPONENTS = List.of(
         BehaviourComponent.class,
         RenderableComponent.class,
-        TransformComponent.class
+        TransformComponent.class,
+        HealthComponent.class
     );
 }
