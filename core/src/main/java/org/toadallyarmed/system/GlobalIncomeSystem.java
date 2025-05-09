e0e37092c6684fa62b5f6ca12a3a0712193218a7
poneciak57 <poneciak@gmail.com>
2025-05-09 14:55:42 +0200

    updated GlobalIncomeSystem to not use GameState directly

--- core/src/main/java/org/toadallyarmed/system/GlobalIncomeSystem.java
+++ core/src/main/java/org/toadallyarmed/system/GlobalIncomeSystem.java
@@ -1,20 +1,21 @@
 package org.toadallyarmed.system;
 
+import org.toadallyarmed.component.WalletComponent;
 import org.toadallyarmed.entity.Entity;
-import org.toadallyarmed.gameplay.GlobalGameState;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class GlobalIncomeSystem implements System {
+    private final WalletComponent globalWallet;
+    private final int globalIncomeDelta;
 
-    private final GlobalGameState gameState;
-
-    GlobalIncomeSystem(GlobalGameState gameState) {
-        this.gameState = gameState;
+    public GlobalIncomeSystem(WalletComponent globalWallet, int globalIncomeDelta) {
+        this.globalWallet = globalWallet;
+        this.globalIncomeDelta = globalIncomeDelta;
     }
 
     @Override
     public void tick(float deltaTime, ConcurrentLinkedQueue<Entity> entities) {
-        gameState.getWallet().access().addAndGet(gameState.getGameConfig().GlobalIncomeDelta());
+        globalWallet.access().addAndGet(globalIncomeDelta);
     }
 }
