631ca941adae0ee27330ed290e1ef3ab54ec9bef
poneciak57 <poneciak@gmail.com>
2025-05-17 15:57:48 +0200

    implemented simple bard action as showcase

---
+++ core/src/main/java/org/toadallyarmed/component/action/payload/BardActionPayload.java
@@ -0,0 +1,21 @@
+package org.toadallyarmed.component.action.payload;
+
+import org.toadallyarmed.component.WalletComponent;
+import org.toadallyarmed.util.action.PayloadExtractor;
+
+import java.util.Optional;
+
+public record BardActionPayload(
+    WalletComponent walletComponent,
+    int bardIncomeDelta
+) {
+    public static final PayloadExtractor<BardActionPayload, BasicActionPayload> EXTRACTOR = new PayloadExtractor<BardActionPayload, BasicActionPayload>() {
+        @Override
+        public Optional<BardActionPayload> extract(BasicActionPayload rawPayload) {
+            return Optional.of(new BardActionPayload(
+                rawPayload.gameState().getWallet(),
+                rawPayload.gameState().getGameConfig().moneyFrog().damage()
+            ));
+        }
+    };
+}
