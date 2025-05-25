90ff062b9d919658d470a1dd3d56c94052b2dbf0
poneciak57 <poneciak@gmail.com>
2025-05-25 19:25:02 +0200

    Fixed bug with throttled system running multiple times at the same time

--- core/src/main/java/org/toadallyarmed/system/ThrottledSystem.java
+++ core/src/main/java/org/toadallyarmed/system/ThrottledSystem.java
@@ -3,12 +3,14 @@ package org.toadallyarmed.system;
 import org.toadallyarmed.entity.Entity;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /// System which tick method is throttled
 public class ThrottledSystem implements System {
     private final System system;
     private final float interval;
     private float accumulatedTime;
+    private final AtomicBoolean running = new AtomicBoolean(false);
 
     public ThrottledSystem(float tickRate, System system) {
         this.interval = 1f / tickRate;
@@ -18,11 +20,14 @@ public class ThrottledSystem implements System {
 
     @Override
     public void tick(float deltaTime, ConcurrentLinkedQueue<Entity> entities) {
+        if (running.get()) return;
         accumulatedTime += deltaTime;
         if (accumulatedTime >= interval) {
             final float finalAccumulatedTime = accumulatedTime;
+            running.set(true);
             accumulatedTime %= interval;
             system.tick(finalAccumulatedTime, entities);
+            running.set(false);
         }
     }
 }
