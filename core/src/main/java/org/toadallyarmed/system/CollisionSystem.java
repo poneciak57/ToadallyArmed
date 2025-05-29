1f6eed2206a390c0bbda77c5aec3ec9f151b915f
poneciak57 <poneciak@gmail.com>
2025-05-29 20:39:01 +0200

    updated CollisionSystem

--- core/src/main/java/org/toadallyarmed/system/CollisionSystem.java
+++ core/src/main/java/org/toadallyarmed/system/CollisionSystem.java
@@ -24,45 +24,48 @@ public class CollisionSystem implements System {
     public void tick(float deltaTime, ConcurrentLinkedQueue<Entity> entities) {
         Logger.trace("CollisionSystem: tick");
         float currentNanoTime = java.lang.System.nanoTime();
-        for (Entity entity : entities) {
-
+        entities.stream()
+            .filter(e -> !e.markedForRemoval())
+            .forEach(entity -> {
             Optional<ColliderComponent> colliderComponent = entity.get(ColliderComponent.class);
             Optional<TransformComponent> transformComponent = entity.get(TransformComponent.class);
             if (colliderComponent.isEmpty() || transformComponent.isEmpty()) {
-                continue;
+                return;
             }
-            for (Entity otherEntity : entities) {
-                if (entity == otherEntity) continue;
-                Optional<ColliderComponent> otherColliderComponent = otherEntity.get(ColliderComponent.class);
-                Optional<TransformComponent> otherTransformComponent = otherEntity.get(TransformComponent.class);
-                if (otherColliderComponent.isEmpty() || otherTransformComponent.isEmpty()) {
-                    continue;
-                }
-                BasicCollisionActionPayload payload = new BasicCollisionActionPayload(
-                   this.gameState,
-                   entity,
-                   otherEntity,
-                   currentNanoTime
-                );
-                for (ColliderActionEntry entry : colliderComponent.get().getEntries()) {
-                    for (ColliderActionEntry otherEntry : otherColliderComponent.get().getEntries()) {
-                        if (!entry.filter(otherEntity.type(), otherEntry.getColliderType())) continue;
-                        boolean intersecting = GJK.intersects(
-                            entry.getShape()
-                                .shiftedBy(
-                                    transformComponent.get()
-                                        .getAdvancedPosition(currentNanoTime)
-                                ),
-                            otherEntry.getShape()
-                                .shiftedBy(
-                                    otherTransformComponent.get()
-                                        .getAdvancedPosition(currentNanoTime)
-                                )
-                        );
-                        if (intersecting) entry.run(currentNanoTime, payload);
+            entities.stream()
+                .filter(e -> !e.markedForRemoval())
+                .forEach(otherEntity -> {
+                    if (entity == otherEntity) return;
+                    Optional<ColliderComponent> otherColliderComponent = otherEntity.get(ColliderComponent.class);
+                    Optional<TransformComponent> otherTransformComponent = otherEntity.get(TransformComponent.class);
+                    if (otherColliderComponent.isEmpty() || otherTransformComponent.isEmpty()) {
+                        return;
                     }
-                }
-            }
-        }
+                    BasicCollisionActionPayload payload = new BasicCollisionActionPayload(
+                        this.gameState,
+                        entity,
+                        otherEntity,
+                        currentNanoTime
+                    );
+                    for (ColliderActionEntry entry : colliderComponent.get().getEntries()) {
+                        for (ColliderActionEntry otherEntry : otherColliderComponent.get().getEntries()) {
+                            if (!entry.filter(otherEntity.type(), otherEntry.getColliderType())) continue;
+                            boolean intersecting = GJK.intersects(
+                                entry.getShape()
+                                    .shiftedBy(
+                                        transformComponent.get()
+                                            .getAdvancedPosition(currentNanoTime)
+                                    ),
+                                otherEntry.getShape()
+                                    .shiftedBy(
+                                        otherTransformComponent.get()
+                                            .getAdvancedPosition(currentNanoTime)
+                                    )
+                            );
+                            if (intersecting) entry.run(currentNanoTime, payload);
+                        }
+                    }
+                });
+        });
     }
 }
